af26c5709c2816cb651044f5d0bbf7f22c36402b
Muhammad Fauzan Al-Rafi <fauzanalrafi16@gmail.com>
2023-11-14 00:01:42 +0700

    first commit

---
+++ first.js
@@ -0,0 +1,29 @@
+
+function tambah(a,b){
+    return a + b
+}
+
+function kurang (a, b){
+    return a - b
+}
+
+function kali (a, b){
+    return a * b
+}
+
+function bagi (a, b){
+    return a/b
+}
+
+a = 6
+b = 2
+
+hasil_tambah = tambah(a, b)
+hasil_kurang = kurang(a, b)
+hasil_kali = kali(a, b)
+hasil_bagi = bagi(a, b)
+
+console.log(hasil_tambah)
+console.log(hasil_kurang)
+console.log(hasil_kali)
+console.log(hasil_bagi)
---
+++ first.py
@@ -0,0 +1,2 @@
+a = "fauzan"
+print(type(a))
